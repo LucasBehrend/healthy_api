6b687c972999021915e1a829cf5917d70424dd37
LucasBehrend <lucasbehrend2006@gmail.com>
2024-05-21 15:46:35 -0300

    intento recibir y enviar requests desde init

--- main_api/__init__.js
+++ main_api/__init__.js
@@ -1,11 +1,12 @@
 import { receive_data, listen } from "./receive_data.js";
-import { requests } from "./send_data.js";
+import requests from "./send_data.js";
 
 const listen_port = 3000;
-const data = requests.receive_data(listen_port);
-switch (data){
+const request = new requests();
+const data = request.receive_data(listen_port);
+switch (data["value"]){
     case 1: 
-        send_request("localhost", 8000, "/", "POST", JSON.stringify({'Content-Type': 'application/json', 'Content-Length': Buffer.byteLength(data)}))
+        request.send_request("localhost", 8000, "/", "POST", JSON.stringify({'Content-Type': 'application/json', 'Content-Length': Buffer.byteLength(data)}))
     defalult:
         console.log("No entra en case 1");
 }
