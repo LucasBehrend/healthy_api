c63c9cd1bf46af83e89a6c68b37fb697c07155a4
LucasBehrend <lucasbehrend2006@gmail.com>
2024-07-04 01:48:22 -0300

    prueba render post turnos2

--- main_api/receive_and_send_express.js
+++ main_api/receive_and_send_express.js
@@ -3,30 +3,30 @@ import axios from 'axios';
 
 class Requests {
     async sendPostRequest(body, options) {
-        try {
+        // try {
             const url = `${options.hostname}`;
-            const response = await axios.post(url, body, {validateStatus: (status) => { return status >= 200 }})
+            const response = await axios.post(url, body)
             console.log(`Status Code: ${response.status}`);
             console.log('Response Body:', response.data);
             return response.data;
-        } 
-        catch (error) {
-            if (error.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
-                console.error('Error Response Status:', error.response.status);
-                console.error('Error Response Headers:', error.response.headers);
-                console.error('Error Response Data:', error.response.data);
-            } else if (error.request) {
-                // The request was made but no response was received
-                console.error('Error Request:', error.request);
-            } else {
-                // Something happened in setting up the request that triggered an Error
-                console.error('Error Message:', error.message);
-            }
-            console.error('Error Config:', error.config);
-            throw error;
-        }
+        // } 
+        // catch (error) {
+        //     if (error.response) {
+        //         // The request was made and the server responded with a status code, {validateStatus: (status) => { return status >= 200 }}
+        //         // that falls out of the range of 2xx
+        //         console.error('Error Response Status:', error.response.status);
+        //         console.error('Error Response Headers:', error.response.headers);
+        //         console.error('Error Response Data:', error.response.data);
+        //     } else if (error.request) {
+        //         // The request was made but no response was received
+        //         console.error('Error Request:', error.request);
+        //     } else {
+        //         // Something happened in setting up the request that triggered an Error
+        //         console.error('Error Message:', error.message);
+        //     }
+        //     console.error('Error Config:', error.config);
+        //     throw error;
+        // }
     }
     async sendGetRequest (path){
         try {
