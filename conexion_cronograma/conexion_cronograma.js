06f6666efae898fbf7d87ba57e6935978873c426
LucasBehrend <lucasbehrend2006@gmail.com>
2024-08-20 15:53:35 -0300

    .

--- conexion_cronograma/conexion_cronograma.js
+++ conexion_cronograma/conexion_cronograma.js
@@ -20,19 +20,6 @@ const port = 8000;
 console.log("conexion crono");
 const url =  process.env.CRONOGRAMA;
 // Define una ruta para la URL raíz ("/") con método POST
-app.post('/', async (req, res) => {
-  let turnos = [];
-  let data = req.body;
-  console.log("post", data);
-  turnos.push({paciente: data.paciente,medico: data.medico, fecha: data.fecha, hora: data.hora});
-
-  const options = request.options(url, "", "POST", {"Content-Type": "application/json"})
-  const response = await request.sendPostRequest(turnos, options);
-  
-  res.status(200);
-  console.log("esto es 2", response);
-  return res.send(response);
-});
 // endpoint get para recibir turnos, enviar parametros en la ruta 
 app.get('/turnos', async (req,res) => {
   console.log("ok");
@@ -48,6 +35,19 @@ app.get('/turnos', async (req,res) => {
     console.log(error.message);
   }
 })
+// app.post('/', async (req, res) => {
+//   let turnos = [];
+//   let data = req.body;
+//   console.log("post", data);
+//   turnos.push({paciente: data.paciente,medico: data.medico, fecha: data.fecha, hora: data.hora});
+
+//   const options = request.options(url, "", "POST", {"Content-Type": "application/json"})
+//   const response = await request.sendPostRequest(turnos, options);
+  
+//   res.status(200);
+//   console.log("esto es 2", response);
+//   return res.send(response);
+// });
 // Inicia el servidor y escucha en el puerto definido
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}/`);
